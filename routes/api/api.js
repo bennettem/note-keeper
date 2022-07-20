fe28d0f429745534cc1a6b0086c14c7e42d1d097
bennettem <emily.pamelabennett@gmail.com>
2022-07-20 08:07:32 -0500

    updated both routes and db file

--- routes/api/api.js
+++ routes/api/api.js
@@ -7,30 +7,34 @@ var notesArray = [];
 
 router.get("/notes", (req, res) => {
     res.sendFile(dbPath);
-    // console.log(res);
+    console.log(res);
 });
 
 router.post("/notes", (req, res) => {
+
     let newNote = {
         title: req.body.title,
         text: req.body.text,
         id: JSON.stringify(notesArray.length)
     };
-    notesArray.push(newNote)
+
+    notesArray.push(newNote);
     content = JSON.stringify(notesArray);
 
     fs.writeFile(dbPath, content, function (err) {
         if (err) throw err;
+        console.log('Saved!');
     });
 
-    res.json(dbPath);
+    res.json(dbFilePath);
+
 });
 
 // delete note 
 
 router.delete("/notes/:id", (req,res) => {
     let deleteNote = req.params.id;
-    let noteIndex = notesArray.findIndex((notes) => notes.id === deleteNote);
+    let noteIndex = notesArray.findIndex(notes => notes.id === deleteNote);
 
     notesArray.splice(noteIndex, 1);
 
@@ -45,3 +49,4 @@ router.delete("/notes/:id", (req,res) => {
 });
 
  module.exports = router;
+
