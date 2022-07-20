6adb8558badafc744ad2789c3014da7572a976f2
bennettem <emily.pamelabennett@gmail.com>
2022-07-19 21:37:21 -0500

    added routes and paths on server

---
+++ routes/api/api.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const router = require("express").Router();
+const dbPath = path.join(__dirname, "../../db/db.json")
+
+var notesArray = [];
+
+router.get("/notes", (req, res) => {
+    res.sendFile(dbPath);
+    console.log(res);
+});
+
+router.post("/notes", (req, res) => {
+    let note = {
+        title: req.body.title,
+        text: req.body.text,
+        id: JSON.stringify(notesArray.length)
+    }
+    notesArray.push(note)
+    content = JSON.stringify(notesArray);
+
+    writeFile(dbPath, content, function (err) {
+        if (err) throw err;
+    });
+
+    res.json(dbPath);
+});
+
+ module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require ("express");
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
+
+const api = require("./routes/api/api");
+const html = require("./routes/html/html");
+
+app.use("/api", api);
+app.use("/", html);
+
+app.use(express.json());
+app.use(express.static("public"));
+
 
 app.listen(PORT, () => {
     console.log(`API server running on ${PORT}!`);
